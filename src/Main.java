4dc2f9a2dab82110681fc0f8908cb39bd0d19762
brait <lorimiervb@gmail.com>
2024-11-01 09:58:21 -0300

    README alterado e menu.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+/*
+Nome: Eduardo Henrique de Souza Cruz RA: 10358690
+Nome: Guilherme Teodoro de Oliveira RA: 10425362
+Nome: Vinícius Brait Lorimier RA: 10420046
+ */
+
+
+import java.awt.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Seja bem-vindo ao nosso Intepretador de Assembly!!!");
+        Menu menu = new Menu();
+        menu.exibeMenu();
+
+        while (true)
+        {
+            System.out.println("\n> ");
+            // validar se a encontrada foi positiva
+        }
     }
 }
